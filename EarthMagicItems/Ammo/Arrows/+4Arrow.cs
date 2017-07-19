432a9789077b60bb31d92d0f0f3cd3ab8c524a92
SneakyTactician <sneakytactician@outlook.com>
2017-07-19 08:25:34 -0500

    Wrote 2 new arrows.

--- EarthMagicItems/Ammo/Arrows/+4Arrow.cs
+++ EarthMagicItems/Ammo/Arrows/+4Arrow.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EarthWithMagicAPI.API;
+using EarthWithMagicAPI.API.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,150 @@ using System.Threading.Tasks;
 
 namespace DungeonsAndFantasyLands.API.Items.Ammo.Arrows
 {
-    class _4Arrow
+    /// <summary>
+    /// A slightly better arrow than the +3 arrows.
+    /// </summary>
+    public class _4Arrow
     {
+        private int _Uses = Dice.RollDice(5, 7);
+        private bool _QuestItem = false;
+        private int _Value = 250;
+        private int _Level = 8;
+        private Guid _ID = new Guid();
+        private string _Name = "Arrow +4";
+        private int _ChanceToHit = 17;
+
+        private List<string> _Lore = new List<string> { };
+        private List<string> _OtherInfo = new List<string> { "Does 1d8 +4 piercing damage.", "This arrow COULD be used 7 times." };
+
+        public _4Arrow()
+        {
+
+        }
+
+        public Damage AttackDamage
+        {
+            get
+            {
+                return new Damage(0, 0, 0, 0, 0, Dice.RollDice(1, 8) + 3, 0, 0);
+            }
+        }
+
+        public int Uses
+        {
+            get
+            {
+                return this._Uses;
+            }
+
+            set
+            {
+                this._Uses = value;
+            }
+        }
+
+        public bool QuestItem
+        {
+            get
+            {
+                return this._QuestItem;
+            }
+
+            set
+            {
+                this._QuestItem = value;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this._Value;
+            }
+
+            set
+            {
+                this._Value = value;
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                return this._Level;
+            }
+
+            set
+            {
+                this._Level = value;
+            }
+        }
+
+        public Guid ID
+        {
+            get
+            {
+                return this._ID;
+            }
+
+            set
+            {
+                this._ID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._Name;
+            }
+
+            set
+            {
+                this._Name = value;
+            }
+        }
+
+        public List<string> Lore
+        {
+            get
+            {
+                return this._Lore;
+            }
+
+            set
+            {
+                this._Lore = value;
+            }
+        }
+
+        public List<string> OtherInformation
+        {
+            get
+            {
+                return this._OtherInfo;
+            }
+
+            set
+            {
+                this._OtherInfo = value;
+            }
+        }
+
+        public int ChanceToHit
+        {
+            get
+            {
+                return this._ChanceToHit;
+            }
+
+            set
+            {
+                this._ChanceToHit = value;
+            }
+        }
     }
 }
--- EarthMagicItems/Ammo/Arrows/+5Arrow.cs
+++ EarthMagicItems/Ammo/Arrows/+5Arrow.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EarthWithMagicAPI.API;
+using EarthWithMagicAPI.API.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,150 @@ using System.Threading.Tasks;
 
 namespace DungeonsAndFantasyLands.API.Items.Ammo.Arrows
 {
-    class _5Arrow
+    /// <summary>
+    /// A slightly better arrow than the +4 arrow.
+    /// </summary>
+    public class _5Arrow
     {
+        private int _Uses = Dice.RollDice(5, 7);
+        private bool _QuestItem = false;
+        private int _Value = 500;
+        private int _Level = 10;
+        private Guid _ID = new Guid();
+        private string _Name = "Arrow +5";
+        private int _ChanceToHit = 18;
+
+        private List<string> _Lore = new List<string> { };
+        private List<string> _OtherInfo = new List<string> { "Does 1d8 +5 piercing damage.", "This arrow COULD be used 7 times." };
+
+        public _5Arrow()
+        {
+
+        }
+
+        public Damage AttackDamage
+        {
+            get
+            {
+                return new Damage(0, 0, 0, 0, 0, Dice.RollDice(1, 8) + 3, 0, 0);
+            }
+        }
+
+        public int Uses
+        {
+            get
+            {
+                return this._Uses;
+            }
+
+            set
+            {
+                this._Uses = value;
+            }
+        }
+
+        public bool QuestItem
+        {
+            get
+            {
+                return this._QuestItem;
+            }
+
+            set
+            {
+                this._QuestItem = value;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return this._Value;
+            }
+
+            set
+            {
+                this._Value = value;
+            }
+        }
+
+        public int Level
+        {
+            get
+            {
+                return this._Level;
+            }
+
+            set
+            {
+                this._Level = value;
+            }
+        }
+
+        public Guid ID
+        {
+            get
+            {
+                return this._ID;
+            }
+
+            set
+            {
+                this._ID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._Name;
+            }
+
+            set
+            {
+                this._Name = value;
+            }
+        }
+
+        public List<string> Lore
+        {
+            get
+            {
+                return this._Lore;
+            }
+
+            set
+            {
+                this._Lore = value;
+            }
+        }
+
+        public List<string> OtherInformation
+        {
+            get
+            {
+                return this._OtherInfo;
+            }
+
+            set
+            {
+                this._OtherInfo = value;
+            }
+        }
+
+        public int ChanceToHit
+        {
+            get
+            {
+                return this._ChanceToHit;
+            }
+
+            set
+            {
+                this._ChanceToHit = value;
+            }
+        }
     }
 }
