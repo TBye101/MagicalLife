f2109fd294c00cd2547f41b369e23ac2cee4e3a4
SneakyTactician <sneakytactician@outlook.com>
2017-07-19 15:50:02 -0500

    Made generic leather armor.

--- EarthMagicItems/Armor/Generic/Steel/GenericSteelArmor.cs
+++ EarthMagicItems/Armor/Generic/Steel/GenericSteelArmor.cs
@@ -11,7 +11,40 @@ namespace EarthMagicItems.Armor.Generic.Steel
     {
         #region Leather
 
+        public GenericArmor LeatherHelmet = new GenericArmor(2, false, 1, new List<string> { }, new List<string> { }, "Leather Helmet");
+        public GenericArmor LeatherHelmet_1 = new GenericArmor(3, false, 2, new List<string> { }, new List<string> { }, "Leather Helmet +1");
+        public GenericArmor LeatherHelmet_2 = new GenericArmor(4, false, 3, new List<string> { }, new List<string> { }, "Leather Helmet +2");
+        public GenericArmor LeatherHelmet_3 = new GenericArmor(5, false, 4, new List<string> { }, new List<string> { }, "Leather Helmet +3");
+        public GenericArmor LeatherHelmet_4 = new GenericArmor(6, false, 5, new List<string> { }, new List<string> { }, "Leather Helmet +4");
+        public GenericArmor LeatherHelmet_5 = new GenericArmor(7, false, 6, new List<string> { }, new List<string> { }, "Leather Helmet +5");
 
+        public GenericArmor LeatherChestplate = new GenericArmor(4, false, 1, new List<string> { }, new List<string> { }, "Leather Chestplate");
+        public GenericArmor LeatherChestplate_1 = new GenericArmor(5, false, 2, new List<string> { }, new List<string> { }, "Leather Chestplate +1");
+        public GenericArmor LeatherChestplate_2 = new GenericArmor(6, false, 3, new List<string> { }, new List<string> { }, "Leather Chestplate +2");
+        public GenericArmor LeatherChestplate_3 = new GenericArmor(7, false, 4, new List<string> { }, new List<string> { }, "Leather Chestplate +3");
+        public GenericArmor LeatherChestplate_4 = new GenericArmor(8, false, 5, new List<string> { }, new List<string> { }, "Leather Chestplate +4");
+        public GenericArmor LeatherChestplate_5 = new GenericArmor(9, false, 6, new List<string> { }, new List<string> { }, "Leather Chestplate +5");
+
+        public GenericArmor LeatherGauntlets = new GenericArmor(1, false, 1, new List<string> { }, new List<string> { }, "Leather Gauntlets");
+        public GenericArmor LeatherGauntlets_1 = new GenericArmor(2, false, 2, new List<string> { }, new List<string> { }, "Leather Gauntlets +1");
+        public GenericArmor LeatherGauntlets_2 = new GenericArmor(3, false, 3, new List<string> { }, new List<string> { }, "Leather Gauntlets +2");
+        public GenericArmor LeatherGauntlets_3 = new GenericArmor(4, false, 4, new List<string> { }, new List<string> { }, "Leather Gauntlets +3");
+        public GenericArmor LeatherGauntlets_4 = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Leather Gauntlets +4");
+        public GenericArmor LeatherGauntlets_5 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Leather Gauntlets +5");
+
+        public GenericArmor LeatherLeggings = new GenericArmor(1, false, 1, new List<string> { }, new List<string> { }, "Leather Leggings");
+        public GenericArmor LeatherLeggings_1 = new GenericArmor(2, false, 2, new List<string> { }, new List<string> { }, "Leather Leggings +1");
+        public GenericArmor LeatherLeggings_2 = new GenericArmor(3, false, 3, new List<string> { }, new List<string> { }, "Leather Leggings +2");
+        public GenericArmor LeatherLeggings_3 = new GenericArmor(4, false, 4, new List<string> { }, new List<string> { }, "Leather Leggings +3");
+        public GenericArmor LeatherLeggings_4 = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Leather Leggings +4");
+        public GenericArmor LeatherLeggings_5 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Leather Leggings +5");
+
+        public GenericArmor LeatherBoots = new GenericArmor(1, false, 1, new List<string> { }, new List<string> { }, "Leather Boots");
+        public GenericArmor LeatherBoots_1 = new GenericArmor(2, false, 2, new List<string> { }, new List<string> { }, "Leather Boots +1");
+        public GenericArmor LeatherBoots_2 = new GenericArmor(3, false, 3, new List<string> { }, new List<string> { }, "Leather Boots +2");
+        public GenericArmor LeatherBoots_3 = new GenericArmor(4, false, 4, new List<string> { }, new List<string> { }, "Leather Boots +3");
+        public GenericArmor LeatherBoots_4 = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Leather Boots +4");
+        public GenericArmor LeatherBoots_5 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Leather Boots +5");
 
         #endregion
 
@@ -24,12 +57,12 @@ namespace EarthMagicItems.Armor.Generic.Steel
         public GenericArmor SteelHelmet_4 = new GenericArmor(7, false, 6, new List<string> { }, new List<string> { }, "Steel Helmet +4");
         public GenericArmor SteelHelmet_5 = new GenericArmor(8, false, 7, new List<string> { }, new List<string> { }, "Steel Helmet +5");
 
-        public GenericArmor SteelChestplate = new GenericArmor(4, false, 2, new List<string> { }, new List<string> { }, "Steel Chestplate");
-        public GenericArmor SteelChestplate_1 = new GenericArmor(5, false, 3, new List<string> { }, new List<string> { }, "Steel Chestplate +1");
-        public GenericArmor SteelChestplate_2 = new GenericArmor(6, false, 4, new List<string> { }, new List<string> { }, "Steel Chestplate +2");
-        public GenericArmor SteelChestplate_3 = new GenericArmor(7, false, 5, new List<string> { }, new List<string> { }, "Steel Chestplate +3");
-        public GenericArmor SteelChestplate_4 = new GenericArmor(8, false, 6, new List<string> { }, new List<string> { }, "Steel Chestplate +4");
-        public GenericArmor SteelChestplate_5 = new GenericArmor(9, false, 7, new List<string> { }, new List<string> { }, "Steel Chestplate +5");
+        public GenericArmor SteelChestplate = new GenericArmor(5, false, 2, new List<string> { }, new List<string> { }, "Steel Chestplate");
+        public GenericArmor SteelChestplate_1 = new GenericArmor(6, false, 3, new List<string> { }, new List<string> { }, "Steel Chestplate +1");
+        public GenericArmor SteelChestplate_2 = new GenericArmor(7, false, 4, new List<string> { }, new List<string> { }, "Steel Chestplate +2");
+        public GenericArmor SteelChestplate_3 = new GenericArmor(8, false, 5, new List<string> { }, new List<string> { }, "Steel Chestplate +3");
+        public GenericArmor SteelChestplate_4 = new GenericArmor(9, false, 6, new List<string> { }, new List<string> { }, "Steel Chestplate +4");
+        public GenericArmor SteelChestplate_5 = new GenericArmor(10, false, 7, new List<string> { }, new List<string> { }, "Steel Chestplate +5");
 
         public GenericArmor SteelGauntlets = new GenericArmor(1, false, 2, new List<string> { }, new List<string> { }, "Steel Gauntlets");
         public GenericArmor SteelGauntlets_1 = new GenericArmor(2, false, 3, new List<string> { }, new List<string> { }, "Steel Gauntlets +1");
@@ -51,6 +84,36 @@ namespace EarthMagicItems.Armor.Generic.Steel
         public GenericArmor SteelBoots_3 = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Steel Boots +3");
         public GenericArmor SteelBoots_4 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Steel Boots +4");
         public GenericArmor SteelBoots_5 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Steel Boots +5");
+
+        #endregion
+
+        #region Tempered Steel
+
+
+
+        #endregion
+
+        #region Mithril
+
+
+
+        #endregion
+
+        #region Tempered Mithril
+
+
+
+        #endregion
+
+        #region Adamantium
+
+
+
+        #endregion
+
+        #region Tempered Adamantium
+
+
 
         #endregion
     }
