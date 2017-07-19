407ccae8eeb1ff40851b1be69f326de8c9018bdd
SneakyTactician <sneakytactician@outlook.com>
2017-07-19 09:47:07 -0500

    Added boots.

---
+++ EarthMagicItems/Armor/Boots/Adamantium/+2AdamantiumBoots.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarthMagicItems.Armor.Boots.Adamantium
+{
+    class _2AdamantiumBoots
+    {
+    }
+}
---
+++ EarthMagicItems/Armor/Boots/Steel/+4SteelBoots.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarthMagicItems.Armor.Boots.Steel
+{
+    class _4SteelBoots
+    {
+    }
+}
