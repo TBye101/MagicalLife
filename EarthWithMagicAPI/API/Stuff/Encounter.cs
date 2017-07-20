877bd216d6c3932ae11d42f544bb6ae75de4c8f8
SneakyTactician <sneakytactician@outlook.com>
2017-07-20 15:12:20 -0500

    Wrote the encounter class.

---
+++ EarthWithMagicAPI/API/Stuff/Encounter.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using EarthWithMagicAPI.API.Creature;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using EarthWithMagicAPI.API.Util;
+
+namespace EarthWithMagicAPI.API.Stuff
+{
+    /// <summary>
+    /// Used whenever a fight occurs.
+    /// </summary>
+    public class Encounter
+    {
+        /// <summary>
+        /// A list of all the combatants sorted by initiative.
+        /// </summary>
+        private List<ICreature> AllCombatants = new List<ICreature>();
+
+        public Encounter(List<ICreature> friendly, List<ICreature> enemies)
+        {
+            AllCombatants.AddRange(friendly);
+            AllCombatants.AddRange(enemies);
+            this.AllCombatants = this.AllCombatants.OrderByDescending(ICreature => ICreature.GetAttributes().Initiative).ToList();
+
+            while (IsEveryoneDead(friendly) == false && IsEveryoneDead(enemies) == false)
+            {
+                Console.WriteLine("Encounter: " + friendly.Count.ToString() + " friendlies, " + enemies.Count.ToString() + " enemies");
+
+                foreach (ICreature item in AllCombatants)
+                {
+                    item.YourTurn();
+                    Util.Util.WriteLine("Hit any key to continue the fight to the next combatant....");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines if everyone on one side is dead yet.
+        /// </summary>
+        /// <param name="creatures"></param>
+        /// <returns></returns>
+        private bool IsEveryoneDead(List<ICreature> creatures)
+        {
+            foreach (ICreature item in creatures)
+            {
+                if (item.GetAttributes().Health > 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
