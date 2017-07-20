dae7b324c3aae962a4e1cb56941cfcc0fab74fb9
SneakyTactician <sneakytactician@outlook.com>
2017-07-19 23:03:09 -0500

    Finished all of the generic armor.

--- EarthMagicItems/Armor/Generic/GenericArmorStorage.cs
+++ EarthMagicItems/Armor/Generic/GenericArmorStorage.cs
@@ -128,25 +128,157 @@ namespace EarthMagicItems.Armor.Generic.Steel
 
         #region Mithril
 
+        public GenericArmor MithrilHelmet = new GenericArmor(5, false, 4, new List<string> { }, new List<string> { }, "Mithril Helmet");
+        public GenericArmor MithrilHelmet_1 = new GenericArmor(6, false, 5, new List<string> { }, new List<string> { }, "Mithril Helmet +1");
+        public GenericArmor MithrilHelmet_2 = new GenericArmor(7, false, 6, new List<string> { }, new List<string> { }, "Mithril Helmet +2");
+        public GenericArmor MithrilHelmet_3 = new GenericArmor(8, false, 7, new List<string> { }, new List<string> { }, "Mithril Helmet +3");
+        public GenericArmor MithrilHelmet_4 = new GenericArmor(9, false, 8, new List<string> { }, new List<string> { }, "Mithril Helmet +4");
+        public GenericArmor MithrilHelmet_5 = new GenericArmor(10, false, 9, new List<string> { }, new List<string> { }, "Mithril Helmet +5");
 
+        public GenericArmor MithrilChestplate = new GenericArmor(7, false, 4, new List<string> { }, new List<string> { }, "Mithril Chestplate");
+        public GenericArmor MithrilChestplate_1 = new GenericArmor(8, false, 5, new List<string> { }, new List<string> { }, "Mithril Chestplate +1");
+        public GenericArmor MithrilChestplate_2 = new GenericArmor(9, false, 7, new List<string> { }, new List<string> { }, "Mithril Chestplate +2");
+        public GenericArmor MithrilChestplate_3 = new GenericArmor(10, false, 8, new List<string> { }, new List<string> { }, "Mithril Chestplate +3");
+        public GenericArmor MithrilChestplate_4 = new GenericArmor(11, false, 9, new List<string> { }, new List<string> { }, "Mithril Chestplate +4");
+        public GenericArmor MithrilChestplate_5 = new GenericArmor(12, false, 10, new List<string> { }, new List<string> { }, "Mithril Chestplate +5");
+
+        public GenericArmor MithrilGauntlets = new GenericArmor(3, false, 4, new List<string> { }, new List<string> { }, "Mithril Gauntlets");
+        public GenericArmor MithrilGauntlets_1 = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Mithril Gauntlets +1");
+        public GenericArmor MithrilGauntlets_2 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Mithril Gauntlets +2");
+        public GenericArmor MithrilGauntlets_3 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Mithril Gauntlets +3");
+        public GenericArmor MithrilGauntlets_4 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Mithril Gauntlets +4");
+        public GenericArmor MithrilGauntlets_5 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Mithril Gauntlets +5");
+
+        public GenericArmor MithrilLeggings = new GenericArmor(4, false, 4, new List<string> { }, new List<string> { }, "Mithril Leggings");
+        public GenericArmor MithrilLeggings_1 = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Mithril Leggings +1");
+        public GenericArmor MithrilLeggings_2 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Mithril Leggings +2");
+        public GenericArmor MithrilLeggings_3 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Mithril Leggings +3");
+        public GenericArmor MithrilLeggings_4 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Mithril Leggings +4");
+        public GenericArmor MithrilLeggings_5 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Mithril Leggings +5");
+
+        public GenericArmor MithrilBoots = new GenericArmor(4, false, 4, new List<string> { }, new List<string> { }, "Mithril Boots");
+        public GenericArmor MithrilBoots_1 = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Mithril Boots +1");
+        public GenericArmor MithrilBoots_2 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Mithril Boots +2");
+        public GenericArmor MithrilBoots_3 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Mithril Boots +3");
+        public GenericArmor MithrilBoots_4 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Mithril Boots +4");
+        public GenericArmor MithrilBoots_5 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Mithril Boots +5");
 
         #endregion
 
         #region Tempered Mithril
 
+        public GenericArmor TemperedMithrilHelmet = new GenericArmor(6, false, 5, new List<string> { }, new List<string> { }, "Tempered Mithril Helmet");
+        public GenericArmor TemperedMithrilHelmet_1 = new GenericArmor(7, false, 6, new List<string> { }, new List<string> { }, "Tempered Mithril Helmet +1");
+        public GenericArmor TemperedMithrilHelmet_2 = new GenericArmor(8, false, 7, new List<string> { }, new List<string> { }, "Tempered Mithril Helmet +2");
+        public GenericArmor TemperedMithrilHelmet_3 = new GenericArmor(9, false, 8, new List<string> { }, new List<string> { }, "Tempered Mithril Helmet +3");
+        public GenericArmor TemperedMithrilHelmet_4 = new GenericArmor(10, false, 9, new List<string> { }, new List<string> { }, "Tempered Mithril Helmet +4");
+        public GenericArmor TemperedMithrilHelmet_5 = new GenericArmor(11, false, 10, new List<string> { }, new List<string> { }, "Tempered Mithril Helmet +5");
 
+        public GenericArmor TemperedMithrilChestplate = new GenericArmor(8, false, 5, new List<string> { }, new List<string> { }, "Tempered Mithril Chestplate");
+        public GenericArmor TemperedMithrilChestplate_1 = new GenericArmor(9, false, 6, new List<string> { }, new List<string> { }, "Tempered Mithril Chestplate +1");
+        public GenericArmor TemperedMithrilChestplate_2 = new GenericArmor(10, false, 7, new List<string> { }, new List<string> { }, "Tempered Mithril Chestplate +2");
+        public GenericArmor TemperedMithrilChestplate_3 = new GenericArmor(11, false, 8, new List<string> { }, new List<string> { }, "Tempered Mithril Chestplate +3");
+        public GenericArmor TemperedMithrilChestplate_4 = new GenericArmor(12, false, 9, new List<string> { }, new List<string> { }, "Tempered Mithril Chestplate +4");
+        public GenericArmor TemperedMithrilChestplate_5 = new GenericArmor(13, false, 10, new List<string> { }, new List<string> { }, "Tempered Mithril Chestplate +5");
+
+        public GenericArmor TemperedMithrilGauntlets = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Tempered Mithril Gauntlets");
+        public GenericArmor TemperedMithrilGauntlets_1 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Tempered Mithril Gauntlets +1");
+        public GenericArmor TemperedMithrilGauntlets_2 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Tempered Mithril Gauntlets +2");
+        public GenericArmor TemperedMithrilGauntlets_3 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Tempered Mithril Gauntlets +3");
+        public GenericArmor TemperedMithrilGauntlets_4 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Tempered Mithril Gauntlets +4");
+        public GenericArmor TemperedMithrilGauntlets_5 = new GenericArmor(10, false, 10, new List<string> { }, new List<string> { }, "Tempered Mithril Gauntlets +5");
+
+        public GenericArmor TemperedMithrilLeggings = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Tempered Mithril Leggings");
+        public GenericArmor TemperedMithrilLeggings_1 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Tempered Mithril Leggings +1");
+        public GenericArmor TemperedMithrilLeggings_2 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Tempered Mithril Leggings +2");
+        public GenericArmor TemperedMithrilLeggings_3 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Tempered Mithril Leggings +3");
+        public GenericArmor TemperedMithrilLeggings_4 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Tempered Mithril Leggings +4");
+        public GenericArmor TemperedMithrilLeggings_5 = new GenericArmor(10, false, 10, new List<string> { }, new List<string> { }, "Tempered Mithril Leggings +5");
+
+        public GenericArmor TemperedMithrilBoots = new GenericArmor(5, false, 5, new List<string> { }, new List<string> { }, "Tempered Mithril Boots");
+        public GenericArmor TemperedMithrilBoots_1 = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Tempered Mithril Boots +1");
+        public GenericArmor TemperedMithrilBoots_2 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Tempered Mithril Boots +2");
+        public GenericArmor TemperedMithrilBoots_3 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Tempered Mithril Boots +3");
+        public GenericArmor TemperedMithrilBoots_4 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Tempered Mithril Boots +4");
+        public GenericArmor TemperedMithrilBoots_5 = new GenericArmor(10, false, 10, new List<string> { }, new List<string> { }, "Tempered Mithril Boots +5");
 
         #endregion
 
         #region Adamantium
 
+        public GenericArmor AdamantiumHelmet = new GenericArmor(7, false, 6, new List<string> { }, new List<string> { }, "Adamantium Helmet");
+        public GenericArmor AdamantiumHelmet_1 = new GenericArmor(8, false, 7, new List<string> { }, new List<string> { }, "Adamantium Helmet +1");
+        public GenericArmor AdamantiumHelmet_2 = new GenericArmor(9, false, 8, new List<string> { }, new List<string> { }, "Adamantium Helmet +2");
+        public GenericArmor AdamantiumHelmet_3 = new GenericArmor(10, false, 9, new List<string> { }, new List<string> { }, "Adamantium Helmet +3");
+        public GenericArmor AdamantiumHelmet_4 = new GenericArmor(11, false, 10, new List<string> { }, new List<string> { }, "Adamantium Helmet +4");
+        public GenericArmor AdamantiumHelmet_5 = new GenericArmor(12, false, 11, new List<string> { }, new List<string> { }, "Adamantium Helmet +5");
 
+        public GenericArmor AdamantiumChestplate = new GenericArmor(9, false, 6, new List<string> { }, new List<string> { }, "Adamantium Chestplate");
+        public GenericArmor AdamantiumChestplate_1 = new GenericArmor(10, false, 7, new List<string> { }, new List<string> { }, "Adamantium Chestplate +1");
+        public GenericArmor AdamantiumChestplate_2 = new GenericArmor(11, false, 8, new List<string> { }, new List<string> { }, "Adamantium Chestplate +2");
+        public GenericArmor AdamantiumChestplate_3 = new GenericArmor(12, false, 9, new List<string> { }, new List<string> { }, "Adamantium Chestplate +3");
+        public GenericArmor AdamantiumChestplate_4 = new GenericArmor(13, false, 10, new List<string> { }, new List<string> { }, "Adamantium Chestplate +4");
+        public GenericArmor AdamantiumChestplate_5 = new GenericArmor(14, false, 11, new List<string> { }, new List<string> { }, "Adamantium Chestplate +5");
+
+        public GenericArmor AdamantiumGauntlets = new GenericArmor(5, false, 6, new List<string> { }, new List<string> { }, "Adamantium Gauntlets");
+        public GenericArmor AdamantiumGauntlets_1 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Adamantium Gauntlets +1");
+        public GenericArmor AdamantiumGauntlets_2 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Adamantium Gauntlets +2");
+        public GenericArmor AdamantiumGauntlets_3 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Adamantium Gauntlets +3");
+        public GenericArmor AdamantiumGauntlets_4 = new GenericArmor(10, false, 10, new List<string> { }, new List<string> { }, "Adamantium Gauntlets +4");
+        public GenericArmor AdamantiumGauntlets_5 = new GenericArmor(11, false, 11, new List<string> { }, new List<string> { }, "Adamantium Gauntlets +5");
+
+        public GenericArmor AdamantiumLeggings = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Adamantium Leggings");
+        public GenericArmor AdamantiumLeggings_1 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Adamantium Leggings +1");
+        public GenericArmor AdamantiumLeggings_2 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Adamantium Leggings +2");
+        public GenericArmor AdamantiumLeggings_3 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Adamantium Leggings +3");
+        public GenericArmor AdamantiumLeggings_4 = new GenericArmor(10, false, 10, new List<string> { }, new List<string> { }, "Adamantium Leggings +4");
+        public GenericArmor AdamantiumLeggings_5 = new GenericArmor(11, false, 11, new List<string> { }, new List<string> { }, "Adamantium Leggings +5");
+
+        public GenericArmor AdamantiumBoots = new GenericArmor(6, false, 6, new List<string> { }, new List<string> { }, "Adamantium Boots");
+        public GenericArmor AdamantiumBoots_1 = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Adamantium Boots +1");
+        public GenericArmor AdamantiumBoots_2 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Adamantium Boots +2");
+        public GenericArmor AdamantiumBoots_3 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Adamantium Boots +3");
+        public GenericArmor AdamantiumBoots_4 = new GenericArmor(10, false, 10, new List<string> { }, new List<string> { }, "Adamantium Boots +4");
+        public GenericArmor AdamantiumBoots_5 = new GenericArmor(11, false, 11, new List<string> { }, new List<string> { }, "Adamantium Boots +5");
 
         #endregion
 
         #region Tempered Adamantium
 
+        public GenericArmor TemperedAdamantiumHelmet = new GenericArmor(8, false, 7, new List<string> { }, new List<string> { }, "Tempered Adamantium Helmet");
+        public GenericArmor TemperedAdamantiumHelmet_1 = new GenericArmor(9, false, 8, new List<string> { }, new List<string> { }, "Tempered Adamantium Helmet +1");
+        public GenericArmor TemperedAdamantiumHelmet_2 = new GenericArmor(10, false, 9, new List<string> { }, new List<string> { }, "Tempered Adamantium Helmet +2");
+        public GenericArmor TemperedAdamantiumHelmet_3 = new GenericArmor(11, false, 10, new List<string> { }, new List<string> { }, "Tempered Adamantium Helmet +3");
+        public GenericArmor TemperedAdamantiumHelmet_4 = new GenericArmor(12, false, 11, new List<string> { }, new List<string> { }, "Tempered Adamantium Helmet +4");
+        public GenericArmor TemperedAdamantiumHelmet_5 = new GenericArmor(13, false, 12, new List<string> { }, new List<string> { }, "Tempered Adamantium Helmet +5");
 
+        public GenericArmor TemperedAdamantiumChestplate = new GenericArmor(10, false, 7, new List<string> { }, new List<string> { }, "Tempered Adamantium Chestplate");
+        public GenericArmor TemperedAdamantiumChestplate_1 = new GenericArmor(11, false, 8, new List<string> { }, new List<string> { }, "Tempered Adamantium Chestplate +1");
+        public GenericArmor TemperedAdamantiumChestplate_2 = new GenericArmor(12, false, 9, new List<string> { }, new List<string> { }, "Tempered Adamantium Chestplate +2");
+        public GenericArmor TemperedAdamantiumChestplate_3 = new GenericArmor(13, false, 10, new List<string> { }, new List<string> { }, "Tempered Adamantium Chestplate +3");
+        public GenericArmor TemperedAdamantiumChestplate_4 = new GenericArmor(14, false, 11, new List<string> { }, new List<string> { }, "Tempered Adamantium Chestplate +4");
+        public GenericArmor TemperedAdamantiumChestplate_5 = new GenericArmor(15, false, 12, new List<string> { }, new List<string> { }, "Tempered Adamantium Chestplate +5");
+
+        public GenericArmor TemperedAdamantiumGauntlets = new GenericArmor(6, false, 7, new List<string> { }, new List<string> { }, "Tempered Adamantium Gauntlets");
+        public GenericArmor TemperedAdamantiumGauntlets_1 = new GenericArmor(10, false, 8, new List<string> { }, new List<string> { }, "Tempered Adamantium Gauntlets +1");
+        public GenericArmor TemperedAdamantiumGauntlets_2 = new GenericArmor(11, false, 9, new List<string> { }, new List<string> { }, "Tempered Adamantium Gauntlets +2");
+        public GenericArmor TemperedAdamantiumGauntlets_3 = new GenericArmor(12, false, 10, new List<string> { }, new List<string> { }, "Tempered Adamantium Gauntlets +3");
+        public GenericArmor TemperedAdamantiumGauntlets_4 = new GenericArmor(13, false, 11, new List<string> { }, new List<string> { }, "Tempered Adamantium Gauntlets +4");
+        public GenericArmor TemperedAdamantiumGauntlets_5 = new GenericArmor(14, false, 12, new List<string> { }, new List<string> { }, "Tempered Adamantium Gauntlets +5");
+
+        public GenericArmor TemperedAdamantiumLeggings = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Tempered Adamantium Leggings");
+        public GenericArmor TemperedAdamantiumLeggings_1 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Tempered Adamantium Leggings +1");
+        public GenericArmor TemperedAdamantiumLeggings_2 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Tempered Adamantium Leggings +2");
+        public GenericArmor TemperedAdamantiumLeggings_3 = new GenericArmor(10, false, 10, new List<string> { }, new List<string> { }, "Tempered Adamantium Leggings +3");
+        public GenericArmor TemperedAdamantiumLeggings_4 = new GenericArmor(11, false, 11, new List<string> { }, new List<string> { }, "Tempered Adamantium Leggings +4");
+        public GenericArmor TemperedAdamantiumLeggings_5 = new GenericArmor(12, false, 12, new List<string> { }, new List<string> { }, "Tempered Adamantium Leggings +5");
+
+        public GenericArmor TemperedAdamantiumBoots = new GenericArmor(7, false, 7, new List<string> { }, new List<string> { }, "Tempered Adamantium Boots");
+        public GenericArmor TemperedAdamantiumBoots_1 = new GenericArmor(8, false, 8, new List<string> { }, new List<string> { }, "Tempered Adamantium Boots +1");
+        public GenericArmor TemperedAdamantiumBoots_2 = new GenericArmor(9, false, 9, new List<string> { }, new List<string> { }, "Tempered Adamantium Boots +2");
+        public GenericArmor TemperedAdamantiumBoots_3 = new GenericArmor(10, false, 10, new List<string> { }, new List<string> { }, "Tempered Adamantium Boots +3");
+        public GenericArmor TemperedAdamantiumBoots_4 = new GenericArmor(11, false, 11, new List<string> { }, new List<string> { }, "Tempered Adamantium Boots +4");
+        public GenericArmor TemperedAdamantiumBoots_5 = new GenericArmor(12, false, 12, new List<string> { }, new List<string> { }, "Tempered Adamantium Boots +5");
 
         #endregion
     }
