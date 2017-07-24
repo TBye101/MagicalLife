3e2e183cbf00d78c4da16dfdde6958bab6d87d67
SneakyTactician <sneakytactician@outlook.com>
2017-07-24 12:36:00 -0500

    Gave the console program a dependency on everything.

--- EarthWithMagic/Program.cs
+++ EarthWithMagic/Program.cs
@@ -7,6 +7,7 @@ namespace EarthWithMagic
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
