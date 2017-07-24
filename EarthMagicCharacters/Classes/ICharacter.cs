3999dc6db35d04763dffd31be968f6c59d9489fd
SneakyTactician <sneakytactician@outlook.com>
2017-07-24 12:34:05 -0500

    Created ICharacter.

---
+++ EarthMagicCharacters/Classes/ICharacter.cs
@@ -0,0 +1,16 @@
+﻿using EarthWithMagicAPI.API.Creature;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarthMagicCharacters.Classes
+{
+    public abstract class ICharacter : ICreature
+    {
+        /// <summary>
+        /// Called whenever it is time to level up this creature.
+        /// The player should control the level up.
+        /// </summary>
+        public abstract void LevelUp();
+    }
+}
---
+++ EarthWithMagicAPI/API/Interfaces/IStory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarthWithMagicAPI.API.Interfaces
+{
+    /// <summary>
+    /// Used so we can detect story modules when doing reflection.
+    /// </summary>
+    public interface IStory
+    {
+        void Start();
+    }
+}
