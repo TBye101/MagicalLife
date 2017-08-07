913645674aa4eacabec3b0da27d9a522dc4047b0
SneakyTactician <sneakytactician@outlook.com>
2017-08-07 10:46:11 -0500

    IWeapon work.

--- EarthWithMagicAPI/API/Interfaces/Items/IWeapon.cs
+++ EarthWithMagicAPI/API/Interfaces/Items/IWeapon.cs
@@ -1,6 +1,25 @@
-﻿namespace EarthWithMagicAPI.API.Interfaces.Items
+﻿using System.Security.Cryptography.X509Certificates;
+namespace EarthWithMagicAPI.API.Interfaces.Items
 {
-    public interface IWeapon
+    /// <summary>
+    /// The interface of all weapons.
+    /// </summary>
+    public abstract class IWeapon
     {
+        /// <summary>
+        /// Called when this weapon is thrown.
+        /// </summary>
+        public abstract void OnThrow();
+
+        /// <summary>
+        /// Called whenever this weapon is attacked with.
+        /// </summary>
+        public abstract void OnAttack();
+
+        /// <summary>
+        /// Called by the creature when it wants to know what this weapon's attack roll is.
+        /// </summary>
+        /// <returns></returns>
+        public abstract Damage Attack();
     }
 }
