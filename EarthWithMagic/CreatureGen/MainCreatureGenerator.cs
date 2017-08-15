88f2a3c0e604cc8787c283b06c6d0c99ac74e4fa
SneakyTactician <sneakytactician@outlook.com>
2017-08-15 08:43:54 -0500

    Fixed the circular dependency.

---
+++ EarthWithMagic/CreatureGen/MainCreatureGenerator.cs
@@ -0,0 +1,29 @@
+﻿using EarthMagicCharacters.Classes.Monk;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarthWithMagicAPI.API.Creature
+{
+    /// <summary>
+    /// Switches and determines which class generator to launch.
+    /// </summary>
+    public class MainCreatureGenerator
+    {
+        public ICreature GetMainCharacter()
+        {
+            Util.Util.WriteLine("Monk?");
+            string input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "monk":
+                    MonkCharacterGeneration gen = new MonkCharacterGeneration();
+                    return gen.Generate();
+                default:
+                    Util.Util.WriteLine("Invalid input!");
+                    return GetMainCharacter();
+            }
+        }
+    }
+}
