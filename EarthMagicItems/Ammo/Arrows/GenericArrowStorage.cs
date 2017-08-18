e2d8301684c6e38d4e8238e06e3f110d0769e140
SneakyTactician <sneakytactician@outlook.com>
2017-08-18 10:17:23 -0500

    Removed excess parameters.

--- EarthMagicItems/Ammo/Arrows/GenericArrowStorage.cs
+++ EarthMagicItems/Ammo/Arrows/GenericArrowStorage.cs
@@ -11,20 +11,20 @@ namespace EarthMagicItems.Ammo.Arrows
     {
         #region NormalArrows
 
-        private GenericAmmo _1Arrow = new GenericAmmo(new Die(2, 4, 0), false, 3, "+1 Arrow", 20, GenericArrowStorage.StandardArrow(new Die(1, 8, 1)), new List<string> { }, new List<string> { });
-        private GenericAmmo _2Arrow = new GenericAmmo(new Die(3, 5, 0), false, 5, "+2 Arrow", 23, GenericArrowStorage.StandardArrow(new Die(1, 8, 2)), new List<string> { }, new List<string> { });
-        private GenericAmmo _3Arrow = new GenericAmmo(new Die(4, 6, 0), false, 7, "+3 Arrow", 26, GenericArrowStorage.StandardArrow(new Die(1, 8, 3)), new List<string> { }, new List<string> { });
-        private GenericAmmo _4Arrow = new GenericAmmo(new Die(5, 7, 0), false, 9, "+4 Arrow", 29, GenericArrowStorage.StandardArrow(new Die(1, 8, 4)), new List<string> { }, new List<string> { });
-        private GenericAmmo _5Arrow = new GenericAmmo(new Die(6, 7, 0), false, 11, "+5 Arrow", 32, GenericArrowStorage.StandardArrow(new Die(1, 8, 5)), new List<string> { }, new List<string> { });
-        private GenericAmmo HandMadeArrow = new GenericAmmo(new Die(1, 2, 0), false, 1, "Handmade Arrow", 10, GenericArrowStorage.StandardArrow(new Die(1, 6, 0)), new List<string> { }, new List<string> { });
-        private GenericAmmo ProfessionalArrow = new GenericAmmo(new Die(1, 3, 0), false, 2, "Professional Arrow", 17, GenericArrowStorage.StandardArrow(new Die(1, 8, 0)), new List<string> { }, new List<string> { });
+        private GenericAmmo _1Arrow = new GenericAmmo(new Die(2, 4, 0), "+1 Arrow", GenericArrowStorage.StandardArrow(new Die(1, 8, 1)), );
+        private GenericAmmo _2Arrow = new GenericAmmo(new Die(3, 5, 0), "+2 Arrow", GenericArrowStorage.StandardArrow(new Die(1, 8, 2)), );
+        private GenericAmmo _3Arrow = new GenericAmmo(new Die(4, 6, 0), "+3 Arrow", GenericArrowStorage.StandardArrow(new Die(1, 8, 3)), );
+        private GenericAmmo _4Arrow = new GenericAmmo(new Die(5, 7, 0), "+4 Arrow", GenericArrowStorage.StandardArrow(new Die(1, 8, 4)), );
+        private GenericAmmo _5Arrow = new GenericAmmo(new Die(6, 7, 0), "+5 Arrow", GenericArrowStorage.StandardArrow(new Die(1, 8, 5)), );
+        private GenericAmmo HandMadeArrow = new GenericAmmo(new Die(1, 2, 0), "Handmade Arrow", GenericArrowStorage.StandardArrow(new Die(1, 6, 0)), );
+        private GenericAmmo ProfessionalArrow = new GenericAmmo(new Die(1, 3, 0), "Professional Arrow", GenericArrowStorage.StandardArrow(new Die(1, 8, 0)), );
 
         #endregion NormalArrows
 
-        private GenericAmmo AcidArrow = new GenericAmmo(new Die(1, 3, 0), false, 3, "Acid Arrow", 17, new Damage(new Die(1, 8, 0), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero()), new List<string> { }, new List<string> { });
-        private GenericAmmo FireArrow = new GenericAmmo(new Die(1, 3, 0), false, 3, "Fire Arrow", 17, new Damage(Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero()), new List<string> { }, new List<string> { });
-        private GenericAmmo FrostArrow = new GenericAmmo(new Die(1, 3, 0), false, 3, "Frost Arrow", 17, new Damage(Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero()), new List<string> { }, new List<string> { });
-        private GenericAmmo PoisonArrow = new GenericAmmo(new Die(1, 3, 0), false, 3, "Poison Arrow", 17, new Damage(Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero()), new List<string> { }, new List<string> { });
+        private GenericAmmo AcidArrow = new GenericAmmo(new Die(1, 3, 0), "Acid Arrow", new Damage(new Die(1, 8, 0), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero()), );
+        private GenericAmmo FireArrow = new GenericAmmo(new Die(1, 3, 0), "Fire Arrow", new Damage(Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero()), );
+        private GenericAmmo FrostArrow = new GenericAmmo(new Die(1, 3, 0), "Frost Arrow", new Damage(Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero()), );
+        private GenericAmmo PoisonArrow = new GenericAmmo(new Die(1, 3, 0), "Poison Arrow", new Damage(Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 8, 0), Die.Zero(), Die.Zero()), );
 
         /// <summary>
         /// Creates a damage object for a arrow that only does normal piercing damage.
