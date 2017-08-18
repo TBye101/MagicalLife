3be476250292237923035acf1a0b9a7957c194bf
SneakyTactician <sneakytactician@outlook.com>
2017-08-18 09:17:45 -0500

    Angel Sword work.

---
+++ EarthMagicItems/Weapons/Normal/Longswords/Angelic/AngelSword.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EarthMagicItems.Weapons.Normal.Longswords.Angelic
+{
+    public class AngelSword
+    {
+    }
+}
