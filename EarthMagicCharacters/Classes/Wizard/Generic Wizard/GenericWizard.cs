bd4b53fab9aa54e3cdfffe9159a9d8d631a3f3a9
SneakyTactician <sneakytactician@outlook.com>
2017-08-21 15:30:46 -0500

    Leveling up for wizards done. Used a new C# 7 feature. Range based switching. Neat.

--- EarthMagicCharacters/Classes/Wizard/Generic Wizard/GenericWizard.cs
+++ EarthMagicCharacters/Classes/Wizard/Generic Wizard/GenericWizard.cs
@@ -1,53 +1,113 @@
 ﻿namespace EarthMagicCharacters.Classes.Wizard.Generic_Wizard
 {
-    using System;
     using EarthWithMagicAPI.API.Creature;
     using EarthWithMagicAPI.API.Interfaces.Items;
     using EarthWithMagicAPI.API.Stuff;
+    using EarthWithMagicAPI.API.Util;
+    using System;
 
     /// <summary>
     /// The base wizard class.
     /// </summary>
-    public class GenericWizard : ICreature
+    public class GenericWizard : ICharacter
     {
-        public GenericWizard(CreatureAttributes attributes, CreatureAbilities abilities, string documentationPath)
-            : base(attributes, abilities, "EarthMagicDocumentation.Classes.Wizard_Class_Info.md", "EarthMagicDocumentation.ASCII_Art.Wizard.txt")
+        public GenericWizard(Gender gender, Race race, Alignment alignment, string name = "Wizard", bool isHostile = false)
+            : base(GetAtt(gender, race, alignment), new CreatureAbilities(), "EarthMagicDocumentation.Classes.Wizard_Class_Info.md", "EarthMagicDocumentation.ASCII_Art.Wizard.txt")
         {
+            this.MaxCastingPower += 5;
         }
 
+        /// <summary>
+        /// Called whenever the creature has a new item equipped.
+        /// </summary>
+        /// <param name="item"></param>
         public override void EquipItem(IItem item)
         {
-            throw new NotImplementedException();
         }
 
         public override bool IsHostile()
         {
-            throw new NotImplementedException();
+            return !this.IsInParty;
+        }
+
+        public override void LevelUp()
+        {
+            this.Attributes.BaseHealth += Dice.RollDice(new Die(1, 4, 1), this.Name + " gains hit points: ");
+            int CastingPower = this.MaxCastingPower;
+            switch (this.Attributes.XP.CreatureLevel)
+            {
+                case int n when (n >= 1 && n <= 4):
+                    this.MaxCastingPower++;
+                    break;
+                case int n when (n >= 5 && n <= 8):
+                    this.MaxCastingPower += 2;
+                    break;
+                case int n when (n >= 9 && n <= 13):
+                    this.MaxCastingPower += 3;
+                    break;
+                case int n when (n >= 14 && n <= 18):
+                    this.MaxCastingPower += 4;
+                    break;
+                case int n when (n >= 19 && n <= 23):
+                    this.MaxCastingPower += 5;
+                    break;
+                case int n when (n >= 24 && n <= 28):
+                    this.MaxCastingPower += 6;
+                    break;
+                case int n when (n >= 29 && n <= 33):
+                    this.MaxCastingPower += 7;
+                    break;
+                case int n when (n >= 34 && n <= 38):
+                    this.MaxCastingPower += 8;
+                    break;
+                case int n when (n >= 39 && n <= 43):
+                    this.MaxCastingPower += 9;
+                    break;
+                case int n when (n >= 44 && n <= 48):
+                    this.MaxCastingPower += 10;
+                    break;
+                case int n when (n == 49 || n == 50):
+                    this.MaxCastingPower += 11;
+                    break;
+                default:
+                    break;
+            }
+
+            Util.WriteLine("Casting power: +" + (this.MaxCastingPower - CastingPower).ToString());
         }
 
         public override void OnCreatureDied(ICreature dead)
         {
-            throw new NotImplementedException();
         }
 
         public override void OnCreatureHealed(ICreature healer)
         {
-            throw new NotImplementedException();
         }
 
         public override void OnDealDamage()
         {
-            throw new NotImplementedException();
         }
 
         public override void OnItemUnequipped(IItem item)
         {
-            throw new NotImplementedException();
         }
 
         public override void YourTurn(Encounter encounter)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generates the base wizard, at character creation.
+        /// </summary>
+        /// <returns></returns>
+        private static CreatureAttributes GetAtt(Gender gender, Race race, Alignment alignment)
+        {
+            int startingHealth = Dice.RollDice(new Die(1, 4, 1), "Starting Health");
+            return new CreatureAttributes(gender, alignment, race, .01, startingHealth, startingHealth,
+            Dice.RollDice(new Die(3, 6, 0), "Dexterity"), Dice.RollDice(new Die(3, 6, 0), "Strength"),
+            Dice.RollDice(new Die(3, 6, 0), "Constitution"), Dice.RollDice(new Die(3, 6, 0), "Charisma"),
+            Dice.RollDice(new Die(3, 6, 0), "Wisdom"), 0, 0, 0, 0, 0, 0, 0, 0, true, 12, .4, 30, Dice.RollDice(new Die(3, 6, 0), "Intelligence"));
         }
     }
 }
