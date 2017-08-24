38385ed875f4f601618447ed6a6106db006b8e89
SneakyTactician <sneakytactician@outlook.com>
2017-08-24 17:00:51 -0500

    Wrote the smiting logic for the holy amulets.

--- EarthMagicItems/Amulets/Clerical/HolySymbolOfTali.cs
+++ EarthMagicItems/Amulets/Clerical/HolySymbolOfTali.cs
@@ -1,4 +1,5 @@
-﻿// <copyright file="HolySymbolOfTali.cs" company="PlaceholderCompany">
+﻿using EarthWithMagicAPI.API;
+// <copyright file="HolySymbolOfTali.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
@@ -58,7 +59,57 @@ namespace EarthMagicItems.Amulets.Clerical
 
         public override void Use(ICreature user, Encounter encounter)
         {
-            throw new NotImplementedException();
+            Damage a = new Damage(Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), new Die((user.Attributes.XP.CreatureLevel / 10) + 1, user.Attributes.XP.CreatureLevel / 2, 1), Die.Zero(), Die.Zero(), Die.Zero());
+            if (user.IsHostile())
+            {
+                foreach (ICreature item in encounter.Party)
+                {
+                    if (item.creatureType == CreatureType.Undead || item.creatureType == CreatureType.Demonic)
+                    {
+                        int chance = Dice.RollDice(new Die(1, 100, 0), "I pray to Tali for a divine smite!");
+
+                        if (this.Smite(user, item))
+                        {
+                            item.RecieveDamage(a);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (ICreature item in encounter.Enemies)
+                {
+                    if (item.creatureType == CreatureType.Undead || item.creatureType == CreatureType.Demonic)
+                    {
+                        int chance = Dice.RollDice(new Die(1, 100, 0), "I pray to Tali for a divine smite!");
+
+                        if (this.Smite(user, item))
+                        {
+                            item.RecieveDamage(a);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Returns whether or not the creature should be smited.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        private bool Smite(ICreature user, ICreature target)
+        {
+            int totalLevels = user.Attributes.XP.CreatureLevel + target.Attributes.XP.CreatureLevel;
+
+            int chance = user.Attributes.XP.CreatureLevel;
+
+            int result = Dice.RollDice(new Die(1, totalLevels, 0), "Chance of smiting!");
+
+            Util.WriteLine("Roll needs to be lower than " + chance.ToString() + " for the smite to work!");
+
+            return result < chance;
         }
 
         public override void WeaponHit(IWeapon attacker)
