889bf3aa29c04d5959ebacfcb63429766475bf5c
SneakyTactician <SneakyTactician@outlook.com>
2017-12-16 16:16:26 -0600

    Updated the .net Core library used to 2.0

--- EarthWithMagicAPI/API/Damage.cs
+++ EarthWithMagicAPI/API/Damage.cs
@@ -1,8 +1,4 @@
-﻿// <copyright file="Damage.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
-// </copyright>
-
-namespace EarthWithMagicAPI.API
+﻿namespace EarthWithMagicAPI.API
 {
     using EarthWithMagicAPI.API.Util;
 
