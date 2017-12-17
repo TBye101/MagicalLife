fc79943393e7a6e3d9b4a8c8fe9a66d66fc914f5
SneakyTactician <SneakyTactician@outlook.com>
2017-12-17 07:56:34 -0600

    Code quality tweak

--- EarthWithMagicAPI/API/Damage.cs
+++ EarthWithMagicAPI/API/Damage.cs
@@ -10,50 +10,50 @@
         /// <summary>
         /// How much acid damage to deal.
         /// </summary>
-        public Die AcidDamage;
+        private Die acidDamage;
 
         /// <summary>
         /// How much blunt damage to deal. (Ex: Rock, club)
         /// </summary>
-        public Die BluntDamage;
+        private Die bluntDamage;
 
         /// <summary>
         /// How much cold damage to deal.
         /// </summary>
-        public Die ColdDamage;
+        private Die coldDamage;
 
         /// <summary>
         /// How much electric damage to deal.s
         /// </summary>
-        public Die ElectricDamage;
+        private Die electricDamage;
 
         /// <summary>
         /// How much fire damage to deal.
         /// </summary>
-        public Die FireDamage;
+        private Die fireDamage;
 
         /// <summary>
         /// How much pure magic damage.
         /// </summary>
-        public Die MagicDamage;
+        private Die magicDamage;
 
         /// <summary>
         /// How much piercing damage to deal. (Ex: Arrows, stabs)
         /// </summary>
-        public Die PiercingDamage;
+        private Die piercingDamage;
 
         /// <summary>
         /// How much poison damage is dealt every turn.
         /// </summary>
-        public Die PoisonDamage;
+        private Die poisonDamage;
 
         /// <summary>
         /// How much slashing damage to deal. (Ex: Sword slashes)
         /// </summary>
-        public Die SlashingDamage;
+        private Die slashingDamage;
 
         /// <summary>
-        /// The constructor for the damage class.
+        /// Initializes a new instance of the <see cref="Damage"/> class.
         /// </summary>
         /// <param name="acidDamage"></param>
         /// <param name="poisonDamage"></param>
@@ -75,6 +75,123 @@
             this.PiercingDamage = piercingDamage;
             this.SlashingDamage = slashingDamage;
             this.BluntDamage = bluntDamage;
+        }
+
+        public Die AcidDamage
+        {
+            get
+            {
+                return this.acidDamage;
+            }
+
+            set
+            {
+                this.acidDamage = value;
+            }
+        }
+
+        public Die BluntDamage
+        {
+            get
+            {
+                return this.bluntDamage;
+            }
+
+            set
+            {
+                this.bluntDamage = value;
+            }
+        }
+
+        public Die ColdDamage
+        {
+            get
+            {
+                return this.coldDamage;
+            }
+
+            set
+            {
+                this.coldDamage = value;
+            }
+        }
+
+        public Die ElectricDamage
+        {
+            get
+            {
+                return this.electricDamage;
+            }
+
+            set
+            {
+                this.electricDamage = value;
+            }
+        }
+
+        public Die FireDamage
+        {
+            get
+            {
+                return this.fireDamage;
+            }
+
+            set
+            {
+                this.fireDamage = value;
+            }
+        }
+
+        public Die MagicDamage
+        {
+            get
+            {
+                return this.magicDamage;
+            }
+
+            set
+            {
+                this.magicDamage = value;
+            }
+        }
+
+        public Die PiercingDamage
+        {
+            get
+            {
+                return this.piercingDamage;
+            }
+
+            set
+            {
+                this.piercingDamage = value;
+            }
+        }
+
+        public Die PoisonDamage
+        {
+            get
+            {
+                return this.poisonDamage;
+            }
+
+            set
+            {
+                this.poisonDamage = value;
+            }
+        }
+
+        public Die SlashingDamage
+        {
+            get
+            {
+                return this.slashingDamage;
+            }
+
+            set
+            {
+                this.slashingDamage = value;
+            }
         }
     }
 }
