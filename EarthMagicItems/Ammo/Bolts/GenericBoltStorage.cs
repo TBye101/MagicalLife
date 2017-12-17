c6c9f1af7cd4d48a55fa513b72e4d3fbdb039094
SneakyTactician <SneakyTactician@outlook.com>
2017-12-17 07:21:22 -0600

    Added the professional bolt item

--- EarthMagicItems/Ammo/Bolts/GenericBoltStorage.cs
+++ EarthMagicItems/Ammo/Bolts/GenericBoltStorage.cs
@@ -13,9 +13,6 @@ namespace EarthMagicItems.Ammo.Bolts
     /// </summary>
     public class GenericBoltStorage
     {
-        private GenericAmmo ProfessionalBolt = new GenericAmmo(new Die(1, 3, 0), "Professional Bolt", AmmoUtil.StandardBolt(new Die(1, 6, 0)), "EarthMagicDocumentation.ASCII_Art.Items.Ammo.CrossbowBolt.txt",
-            "EarthMagicDocumentation.Items.Ammo.Bolts.ProfessionalBolt.md", .15);
-
         private GenericAmmo AcidBolt = new GenericAmmo(new Die(1, 3, 0), "Acid Bolt", new Damage(new Die(1, 8, 0), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), Die.Zero(), new Die(1, 6, 0), Die.Zero(), Die.Zero()),
             "EarthMagicDocumentation.ASCII_Art.Items.Ammo.CrossbowBolt.txt",
             "EarthMagicDocumentation.Items.Ammo.Bolts.AcidBolt.md", .15);
