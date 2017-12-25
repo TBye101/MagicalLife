7e135dba32d1ed170edc941a48fd4d0dcdd44f65
SneakyTactician <SneakyTactician@outlook.com>
2017-12-25 15:46:20 -0600

    Working on alignments and alignment shifting. Need to keep a history of choices that impact alignment.

---
+++ DungeonsOfTheGodsAPI/DND5E/Creatures/Alignment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsOfTheGodsAPI.DND5E.Creatures
+{
+    /// <summary>
+    /// Used to handle alignment, and the shifts between alignments.
+    /// </summary>
+    public class Alignment
+    {
+        /// <summary>
+        /// This is the value behind whether or not the creature is Lawful, chaotic, or neutral.
+        /// </summary>
+        public int Ethics { get; set; }
+
+        /// <summary>
+        /// This is the value behind whether or not the creature is good, neutral, or evil.
+        /// </summary>
+        public int Morals { get; set; }
+
+        public Alignment()
+        {
+
+        }
+
+        public enum AlignmentNames
+        {
+            LawfulGood, NeutralGood, ChaoticGood, LawfulNeutral, TrueNeutral, ChaoticNeutral, LawfulEvil, NeutralEvil, ChaoticEvil
+        }
+    }
+}
---
+++ DungeonsOfTheGodsAPI/DND5E/Creatures/AlignmentValues.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsOfTheGodsAPI.DND5E.Creatures
+{
+    /// <summary>
+    /// Used to hold two values about alignment: ethics, and morals.
+    /// </summary>
+    public class AlignmentValues
+    {
+        /// <summary>
+        /// Holds a value which describes a creature's tendencies between order and chaos.
+        /// </summary>
+        public int Ethics { get; set; }
+
+        /// <summary>
+        /// Holds a value which describes a creature's tendencies between good and evil.
+        /// </summary>
+        public int Morals { get; set; }
+
+        /// <summary>
+        /// Constructs the <see cref="AlignmentValues"/> class.
+        /// </summary>
+        /// <param name="ethics"></param>
+        /// <param name="morals"></param>
+        public AlignmentValues(int ethics, int morals)
+        {
+            this.Ethics = ethics;
+            this.Morals = morals;
+        }
+    }
+}
