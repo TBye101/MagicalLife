fd238ee1c55eecb84a13073852de50289a3c7a81
SneakyTactician <SneakyTactician@outlook.com>
2017-12-25 18:21:29 -0600

    Working hard on alignment.

--- DungeonsOfTheGodsAPI/DND5E/Creatures/Alignment.cs
+++ DungeonsOfTheGodsAPI/DND5E/Creatures/Alignment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,24 +10,89 @@ namespace DungeonsOfTheGodsAPI.DND5E.Creatures
     /// </summary>
     public class Alignment
     {
-        /// <summary>
-        /// This is the value behind whether or not the creature is Lawful, chaotic, or neutral.
-        /// </summary>
-        public int Ethics { get; set; }
+        private AlignmentValue averageAlignment;
+        private int HistorySize = 1;
 
         /// <summary>
-        /// This is the value behind whether or not the creature is good, neutral, or evil.
+        /// The average choices of the creature between chaos, order, good, and evil.
         /// </summary>
-        public int Morals { get; set; }
-
-        public Alignment()
+        public AlignmentValue AverageAlignment
         {
+            get
+            {
+                return this.averageAlignment;
+            }
+        }
 
+        public Alignment(AlignmentNames initial)
+        {
+            this.averageAlignment = this.GetValueForName(initial);
+        }
+
+        private AlignmentValue GetValueForName(AlignmentNames name)
+        {
+            switch (name)
+            {
+                case AlignmentNames.LawfulGood:
+                    return new AlignmentValue(100, 100);
+                case AlignmentNames.NeutralGood:
+                    return new AlignmentValue(65, 100);
+                case AlignmentNames.ChaoticGood:
+                    return new AlignmentValue(0, 100);
+                case AlignmentNames.LawfulNeutral:
+                    return new AlignmentValue(100, 65);
+                case AlignmentNames.TrueNeutral:
+                    return new AlignmentValue(50, 50);
+                case AlignmentNames.ChaoticNeutral:
+                    return new AlignmentValue(0, 35);
+                case AlignmentNames.LawfulEvil:
+                    return new AlignmentValue(100, 0);
+                case AlignmentNames.NeutralEvil:
+                    return new AlignmentValue(35, 0);
+                case AlignmentNames.ChaoticEvil:
+                    return new AlignmentValue(0, 0);
+            }
+
+            return new AlignmentValue(-1, -1);
         }
 
         public enum AlignmentNames
         {
             LawfulGood, NeutralGood, ChaoticGood, LawfulNeutral, TrueNeutral, ChaoticNeutral, LawfulEvil, NeutralEvil, ChaoticEvil
+        }
+
+        /// <summary>
+        /// Adds the impact of a alignment based choice to our current alignment and recalculates the player's alignment.
+        /// </summary>
+        /// <param name="newChoice"></param>
+        public void AddAlignmentValue(AlignmentValue newChoice)
+        {
+            this.averageAlignment = this.CalculateAlignmentValueAverage(newChoice);
+        }
+
+        /// <summary>
+        /// Calculates the creature's average choices between good, evil, order, and chaos.
+        /// </summary>
+        /// <returns></returns>
+        private AlignmentValue CalculateAlignmentValueAverage(AlignmentValue newChoices)
+        {
+            int morals = this.AddToAverage(this.AverageAlignment.Morals, this.HistorySize, newChoices.Morals);
+            int ethics = this.AddToAverage(this.AverageAlignment.Ethics, this.HistorySize, newChoices.Ethics);
+            this.HistorySize++;
+
+            return new AlignmentValue(ethics, morals);
+        }
+
+        /// <summary>
+        /// Adds an item to a preaveraged average.
+        /// </summary>
+        /// <param name="average"></param>
+        /// <param name="size"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private int AddToAverage(double average, int size, double value)
+        {
+            return (int)Math.Round(((size * average) + value) / (size + 1));
         }
     }
 }
--- DungeonsOfTheGodsAPI/DND5E/Creatures/AlignmentValue.cs
+++ DungeonsOfTheGodsAPI/DND5E/Creatures/AlignmentValue.cs
@@ -7,7 +7,7 @@ namespace DungeonsOfTheGodsAPI.DND5E.Creatures
     /// <summary>
     /// Used to hold two values about alignment: ethics, and morals.
     /// </summary>
-    public class AlignmentValues
+    public class AlignmentValue
     {
         /// <summary>
         /// Holds a value which describes a creature's tendencies between order and chaos.
@@ -20,11 +20,11 @@ namespace DungeonsOfTheGodsAPI.DND5E.Creatures
         public int Morals { get; set; }
 
         /// <summary>
-        /// Constructs the <see cref="AlignmentValues"/> class.
+        /// Constructs the <see cref="AlignmentValue"/> class.
         /// </summary>
         /// <param name="ethics"></param>
         /// <param name="morals"></param>
-        public AlignmentValues(int ethics, int morals)
+        public AlignmentValue(int ethics, int morals)
         {
             this.Ethics = ethics;
             this.Morals = morals;
