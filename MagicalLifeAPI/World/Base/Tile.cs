6fbd30426a97f3b6f4f37ab2d7b850c41be26ba4
SneakyTactician <SneakyTactician@outlook.com>
2018-01-06 14:19:50 -0600

    Commented out some more advanced stuff for later.

--- MagicalLifeAPI/World/Base/Tile.cs
+++ MagicalLifeAPI/World/Base/Tile.cs
@@ -10,47 +10,47 @@ namespace MagicalLifeAPI.World
 {
     public abstract class Tile : Unique
     {
-        /// <summary>
-        /// The loss of movement by stepping on this tile.
-        /// </summary>
-        public double MovementCost
-        {
-            get
-            {
-                return this.LastMovementCost;
-            }
-        }
+        ///// <summary>
+        ///// The loss of movement by stepping on this tile.
+        ///// </summary>
+        //public double MovementCost
+        //{
+        //    get
+        //    {
+        //        return this.LastMovementCost;
+        //    }
+        //}
 
-        /// <summary>
-        /// The additional movement cost that the terrain of the tile adds to the total movement cost of moving here.
-        /// </summary>
-        protected double AdditionalMovementCost { get; set; }
+        ///// <summary>
+        ///// The additional movement cost that the terrain of the tile adds to the total movement cost of moving here.
+        ///// </summary>
+        //protected double AdditionalMovementCost { get; set; }
 
-        /// <summary>
-        /// The last calculated movement cost of this tile.
-        /// </summary>
-        private double LastMovementCost { get; set; }
+        ///// <summary>
+        ///// The last calculated movement cost of this tile.
+        ///// </summary>
+        //private double LastMovementCost { get; set; }
 
         /// <summary>
         /// Returns the name of the biome that this tile belongs to.
         /// </summary>
         public string BiomeName { get; }
 
-        /// <summary>
-        /// Returns the last calculated value of the temperature of this tile.
-        /// </summary>
-        public double Temperature
-        {
-            get
-            {
-                return this.LastTemperature;
-            }
-        }
+        ///// <summary>
+        ///// Returns the last calculated value of the temperature of this tile.
+        ///// </summary>
+        //public double Temperature
+        //{
+        //    get
+        //    {
+        //        return this.LastTemperature;
+        //    }
+        //}
 
-        /// <summary>
-        /// The last calculated temperature value of this tile.
-        /// </summary>
-        private double LastTemperature { get; set; }
+        ///// <summary>
+        ///// The last calculated temperature value of this tile.
+        ///// </summary>
+        //private double LastTemperature { get; set; }
 
         /// <summary>
         /// The resources that can be found in this tile.
