691a3257b3bae244e220957cb22b8324ca43a6e3
SneakyTactician <SneakyTactician@outlook.com>
2018-01-05 23:22:03 -0600

    Defined many more attributes of vegetation.

--- MagicalLifeAPI/World/Vegetation.cs
+++ MagicalLifeAPI/World/Vegetation.cs
@@ -20,6 +20,51 @@ namespace MagicalLifeAPI.World
         private double PercentGrown { get; set; }
 
         /// <summary>
+        /// The minimum amount of precipitation for this plant to survive.
+        /// </summary>
+        public double PrecipitationMin { get; set; }
+
+        /// <summary>
+        /// The maximum amount of precipitation that this plant can survive.
+        /// </summary>
+        public double Precipitationmax { get; set; }
+
+        /// <summary>
+        /// The chance that this vegetation will reproduce and create a immature version of itself nearby.
+        /// </summary>
+        public double ReproductionChance { get; set; }
+
+        /// <summary>
+        /// The minimum amount of offspring this vegetation is allowed to create.
+        /// </summary>
+        public int OffSpringMin { get; set; }
+
+        /// <summary>
+        /// The maximum amount of offspring this vegetation is allowed to create.
+        /// </summary>
+        public int OffSpringMax { get; set; }
+
+        /// <summary>
+        /// The minimum amount of days between reproduction attempts.
+        /// </summary>
+        private int MinDaysBetweenReproduction { get; }
+
+        /// <summary>
+        /// If true, this vegetation is dormant. This is most likely due to winter.
+        /// </summary>
+        public bool IsDorment { get; set; }
+
+        /// <summary>
+        /// The range at which this plant could possibly create a immature version of itself.
+        /// </summary>
+        public int ReproductionRange { get; set; }
+
+        /// <summary>
+        /// The chance that the offspring of this vegetation will be slightly different due to a mutation.
+        /// </summary>
+        public double MutationChance { get; set; }
+
+        /// <summary>
         /// Returns the percent that this vegetation has currently grown.
         /// </summary>
         /// <returns></returns>
