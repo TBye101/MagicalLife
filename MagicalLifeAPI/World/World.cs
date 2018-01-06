ee37c565324ccde8a64674c108e5ed25fca5b41d
SneakyTactician <SneakyTactician@outlook.com>
2018-01-06 14:58:21 -0600

    Wrote some stuff to generate the world.

--- MagicalLifeAPI/World/World.cs
+++ MagicalLifeAPI/World/World.cs
@@ -1,4 +1,5 @@
-﻿using MagicalLifeAPI.Universal;
+﻿using System.Security.Cryptography;
+using MagicalLifeAPI.Universal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,5 +17,38 @@ namespace MagicalLifeAPI.World
         /// A 3D array that holds every tile in the current world.
         /// </summary>
         public Tile[,,] Tiles { get; }
+
+        /// <summary>
+        /// Generates a new world with the specified height, width, depth, and world generator.
+        /// </summary>
+        /// <param name="height"></param>
+        /// <param name="width"></param>
+        /// <param name="depth"></param>
+        /// <param name="generator"></param>
+        public World(int height, int width, int depth, WorldGenerator generator)
+        {
+            this.Tiles = this.GenerateWorld(height, width, depth, generator);
+        }
+
+        /// <summary>
+        /// Actually handles generating the world.
+        /// </summary>
+        /// <param name="height"></param>
+        /// <param name="width"></param>
+        /// <param name="depth"></param>
+        /// <param name="generator"></param>
+        /// <returns></returns>
+        private Tile[,,] GenerateWorld(int height, int width, int depth, WorldGenerator generator)
+        {
+            string[,,] stage1 = generator.AssignBiomes(height, width, depth);
+            Tile[,,] stage2 = generator.GenerateLandType(stage1);
+
+            stage2 = generator.GenerateNaturalFeatures(stage2);
+            stage2 = generator.GenerateMinerals(stage2);
+            stage2 = generator.GenerateVegetation(stage2);
+            stage2 = generator.GenerateDetails(stage2);
+
+            return stage2;
+        }
     }
 }
