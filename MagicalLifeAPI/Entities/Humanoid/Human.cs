03d580d31ae1926338d4d1292863767f23f2785e
SneakyTactician <SneakyTactician@outlook.com>
2018-01-15 14:02:42 -0600

    Added the human texture.

---
+++ MagicalLifeAPI/Entities/Humanoid/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalLifeAPI.Entities.Humanoid
+{
+    /// <summary>
+    /// A class that holds logic for control of regular humans.
+    /// </summary>
+    public class Human : Living
+    {
+        public Human(int health, int movementSpeed) : base(health, movementSpeed)
+        {
+        }
+    }
+}
--- MagicalLifeAPI/Util/StaticRandom.cs
+++ MagicalLifeAPI/Util/StaticRandom.cs
@@ -10,6 +10,12 @@ namespace MagicalLifeAPI.Util
         private static readonly ThreadLocal<Random> random =
             new ThreadLocal<Random>(() => new Random(Interlocked.Increment(ref seed)));
 
+        /// <summary>
+        /// Returns a random number.
+        /// </summary>
+        /// <param name="min">Inclusive minimum.</param>
+        /// <param name="max">Exclusive maximum.</param>
+        /// <returns></returns>
         public static int Rand(int min, int max)
         {
             return random.Value.Next(min, max);
