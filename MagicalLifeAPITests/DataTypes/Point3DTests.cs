fad41888b2fec37857574949f7271994598549e9
SneakyTactician <SneakyTactician@outlook.com>
2018-02-03 23:56:22 -0600

    Added a new test project.

---
+++ MagicalLifeAPITests/DataTypes/Point3DTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MagicalLifeAPI.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalLifeAPI.DataTypes.Tests
+{
+    [TestClass()]
+    public class Point3DTests
+    {
+        [TestMethod()]
+        public void Point3DTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
