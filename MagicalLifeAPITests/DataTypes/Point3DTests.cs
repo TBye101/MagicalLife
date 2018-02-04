0657a7d12caac7a97cc2e3a4f45bd6f1134b4616
SneakyTactician <SneakyTactician@outlook.com>
2018-02-04 00:01:13 -0600

    Wrote some tests for the Point3D class.

--- MagicalLifeAPITests/DataTypes/Point3DTests.cs
+++ MagicalLifeAPITests/DataTypes/Point3DTests.cs
@@ -14,7 +14,27 @@ namespace MagicalLifeAPI.DataTypes.Tests
         [TestMethod()]
         public void Point3DTest()
         {
-            Assert.Fail();
+            Point3D a = new Point3D(3, 82, 3);
+            Assert.AreEqual(a.X, 3);
+            Assert.AreEqual(a.Y, 82);
+            Assert.AreEqual(a.Z, 3);
+        }
+
+        [TestMethod()]
+        public void Point3DTest1()
+        {
+            Point3D a = new Point3D("3, 2, 1");
+            Assert.AreEqual(a.X, 3);
+            Assert.AreEqual(a.Y, 2);
+            Assert.AreEqual(a.Z, 1);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Point3D a = new Point3D(3, 2, 1);
+            string test = a.ToString();
+            Assert.AreEqual("3, 2, 1", test);
         }
     }
 }
