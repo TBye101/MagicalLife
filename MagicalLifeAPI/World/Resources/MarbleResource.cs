63c2b8f6087b99c53f94c8ffb571fc865fc16aea
SneakyTactician <SneakyTactician@outlook.com>
2018-03-30 18:33:26 -0500

    Added textures for all the states of the marble resource.

--- MagicalLifeAPI/World/Resources/MarbleResource.cs
+++ MagicalLifeAPI/World/Resources/MarbleResource.cs
@@ -9,10 +9,35 @@ namespace MagicalLifeAPI.World.Resources
     /// <summary>
     /// Stone as a resource.
     /// </summary>
-    public class MarbleResource : Resource
+    public class MarbleResource : StoneBase
     {
         public MarbleResource(int count) : base("Marble", count)
         {
+        }
+
+        public override string GetConnectedFourTexture()
+        {
+            return "MarbleResourceConnected4";
+        }
+
+        public override string GetConnectedOneTexture()
+        {
+            return "MarbleResourceConnected1";
+        }
+
+        public override string GetConnectedThreeTexture()
+        {
+            return "MarbleResourceConnected3";
+        }
+
+        public override string GetConnectedTwoTexture()
+        {
+            return "MarbleResourceConnected2";
+        }
+
+        public override string GetUnconnectedTexture()
+        {
+            return "MarbleResourceUnconnected";
         }
     }
 }
---
+++ MagicalLifeAPI/World/Resources/StoneBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalLifeAPI.World.Resources
+{
+    /// <summary>
+    /// The base class for all stone-like resources.
+    /// </summary>
+    public abstract class StoneBase : Resource
+    {
+        public StoneBase(string name, int count) : base(name, count)
+        {
+        }
+
+        /// <summary>
+        /// Gets the name of the texture to use when this stone is not adjacent to any other tile with a stone resource.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetUnconnectedTexture();
+
+        /// <summary>
+        /// Gets the name of the texture to use when this stone is connected to another stone.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetConnectedOneTexture();
+
+        /// <summary>
+        /// Gets the name of the texture to use when this stone is connected to two stone resources in adjacent tiles.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetConnectedTwoTexture();
+
+        /// <summary>
+        /// Gets the name of the texture to use when this stone is connected to three stone resources in adjacent tiles.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetConnectedThreeTexture();
+
+        /// <summary>
+        /// Gets the name of the texture to use when this stone is connected to four stone resources in adjacent tiles.
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetConnectedFourTexture();
+    }
+}
