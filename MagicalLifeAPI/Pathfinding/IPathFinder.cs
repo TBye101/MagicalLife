def548870b64da08d74e4430e7aad1e8fa28aa2c
SneakyTactician <SneakyTactician@outlook.com>
2018-04-05 15:46:07 -0500

    Beginning preliminary work on a path finding algorithm.

---
+++ MagicalLifeAPI/Pathfinding/IPathFinder.cs
@@ -0,0 +1,26 @@
+﻿using MagicalLifeAPI.Entities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalLifeAPI.Pathfinding
+{
+    /// <summary>
+    /// All pathfinding algorithms must implement this.
+    /// </summary>
+    public interface IPathFinder
+    {
+        /// <summary>
+        /// Must get a valid path between the origin and the destination.
+        /// </summary>
+        /// <param name="world">The world with which the <see cref="Living"/> exists within.</param>
+        /// <param name="living">The creature which will move between the two points.</param>
+        /// <param name="destination">The target location for the living to reach.</param>
+        /// <param name="origin">The starting point of the living.</param>
+        /// <returns></returns>
+        List<PathLink> GetRoute(World.World world, Living living, Point origin, Point destination);
+    }
+}
