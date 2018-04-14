911bbcd0cd121b9408d668d8ddfbcd2661067b5a
SneakyTactician <SneakyTactician@outlook.com>
2018-04-14 11:53:51 -0500

    Client server connected.

---
+++ MagicalLifeClient/Networking/TCPClient.cs
@@ -0,0 +1,23 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalLifeClient.Networking
+{
+    /// <summary>
+    /// The TCP client for communicating with the server.
+    /// </summary>
+    public class TCPClient
+    {
+        public SimpleTcpClient Client;
+
+        public void Start(int port, string ip = "192.168.0.10")
+        {
+            this.Client = new SimpleTcpClient();
+            this.Client.Connect(ip, port);
+        }
+    }
+}
--- MagicalLifeGUIWindows/Game1.cs
+++ MagicalLifeGUIWindows/Game1.cs
@@ -48,8 +48,10 @@ namespace MagicalLifeGUIWindows
             winConfig.ConfigureMainWindow(this);
 
             Universal.Default.GameHasRunBefore = true;
-            //MagicalLifeServer.Networking.TCPServer tCPServer = new MagicalLifeServer.Networking.TCPServer();
-            //tCPServer.Start(5849);
+            MagicalLifeServer.Networking.TCPServer tCPServer = new MagicalLifeServer.Networking.TCPServer();
+            tCPServer.Start(5849);
+            MagicalLifeClient.Networking.TCPClient tCPClient = new MagicalLifeClient.Networking.TCPClient();
+            tCPClient.Start(5849);
         }
 
         /// <summary>
