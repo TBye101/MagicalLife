0ad560bbf8876cbbb6f7a7af3b6a0dcf2b6a40c7
SneakyTactician <SneakyTactician@outlook.com>
2018-04-14 11:04:07 -0500

    Added a new project that will store various classes that deal with storing information, packaging, and depackaging client/server messages.

---
+++ MagicalLifeServer/Networking/TCPServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalLifeServer.Networking
+{
+    /// <summary>
+    /// The TCP server for communicating with clients.
+    /// </summary>
+    public class TCPServer
+    {
+        
+    }
+}
