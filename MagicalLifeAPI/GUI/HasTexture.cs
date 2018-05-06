06eb69c3c1ade8d8a9827d12d2aa4504ab68ee78
Thomas Bye <sneakytactician@outlook.com>
2018-05-06 10:22:20 -0500

    Texture index now serializing.

--- MagicalLifeAPI/GUI/HasTexture.cs
+++ MagicalLifeAPI/GUI/HasTexture.cs
@@ -1,15 +1,19 @@
 ﻿using MagicalLifeAPI.Universal;
+using MagicalLifeAPI.World;
 
 namespace MagicalLifeAPI.GUI
 {
     /// <summary>
     /// Any class that inherits from this has a texture.
     /// </summary>
+    [ProtoBuf.ProtoContract]
+    [ProtoBuf.ProtoInclude(1, typeof(Tile))]
     public class HasTexture : Unique
     {
         /// <summary>
         /// The index of the texture in our asset manager.
         /// </summary>
+        [ProtoBuf.ProtoMember(2)]
         public int TextureIndex { get; set; }
 
         public HasTexture(int textureIndex)
