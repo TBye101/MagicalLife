88ea9dab47f63855dd3f34972a88e3cb630bb051
Thomas Bye <sneakytactician@outlook.com>
2018-07-23 23:19:42 -0500

    Last attempt

--- MagicalLifeAPI/Sound/FMODUtil.cs
+++ MagicalLifeAPI/Sound/FMODUtil.cs
@@ -3,6 +3,7 @@ using MagicalLifeAPI.Filing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,13 +17,36 @@ namespace MagicalLifeAPI.Sound
         public static void Test()
         {
             FMOD.RESULT result7 = FMOD.Studio.System.create(out FMOD.Studio.System a);
+            a.getLowLevelSystem(out FMOD.System low);
+            FMOD.RESULT result11 = low.setSoftwareFormat(0, FMOD.SPEAKERMODE._7POINT1, 0);
             FMOD.RESULT result6 = a.initialize(1, INITFLAGS.NORMAL, FMOD.INITFLAGS.NORMAL, IntPtr.Zero);
-            FMOD.RESULT result = a.loadBankFile(FileSystemManager.RootDirectory + "/Content/Banks/Master Bank.bank", FMOD.Studio.LOAD_BANK_FLAGS.NORMAL, out Bank testBank);
+            FMOD.RESULT result = a.loadBankFile(FileSystemManager.RootDirectory + "/Content/Banks/Master_Bank.bank", FMOD.Studio.LOAD_BANK_FLAGS.NORMAL, out Bank testBank);
+            FMOD.RESULT result10 = a.loadBankFile(FileSystemManager.RootDirectory + "/Content/Banks/Master_Bank.strings.bank", LOAD_BANK_FLAGS.NORMAL, out Bank bank);
+
+
+            FMOD.RESULT result9 = testBank.loadSampleData();
             FMOD.RESULT result5 = testBank.getEventList(out EventDescription[] someArray);
-            FMOD.RESULT result8 = someArray[0].loadSampleData();
+            //FMOD.RESULT result12 = someArray[0].setCallback(testmethod, EVENT_CALLBACK_TYPE.STARTED);
+            //FMOD.RESULT result8 = someArray[0].loadSampleData();
             FMOD.RESULT result4 = someArray[0].createInstance(out EventInstance instance);
             FMOD.RESULT result3 = instance.setVolume(1F);
             FMOD.RESULT result2 = instance.start();
+            //FMOD.Studio.EVENT_CALLBACK
         }
+
+        //private static FMOD.RESULT testmethod(FMOD.Studio.EVENT_CALLBACK_TYPE type, IntPtr eventInstance, IntPtr parameters)
+        //{
+        //    GCHandle handle = (GCHandle)eventInstance;
+        //    object target = handle.Target;
+        //    EventInstance instance = (EventInstance)target;
+
+        //    switch (type)
+        //    {
+        //        case EVENT_CALLBACK_TYPE.STARTED:
+        //            instance.setVolume(1F);
+        //            return instance.start();
+        //    }
+        //    return FMOD.RESULT.ERR_UNIMPLEMENTED;
+        //}
     }
 }
