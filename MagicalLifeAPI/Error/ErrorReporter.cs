61545cace9f28edd7cefccf68cff29207eec0ed9
Thomas Bye <ThomasBye1@outlook.com>
2018-08-23 13:51:12 -0500

    Now auto breaking when debugging.

--- MagicalLifeAPI/Error/ErrorReporter.cs
+++ MagicalLifeAPI/Error/ErrorReporter.cs
@@ -1,6 +1,7 @@
 ﻿using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,21 @@ namespace MagicalLifeAPI.Error
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             Log.Error((Exception)e.ExceptionObject, "Unhandled exception: ");
+
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
         }
 
         private static void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
         {
             Log.Error(e.Exception, "First chance exception: ");
+
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
         }
     }
 }
