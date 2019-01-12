96113da1ab13b49d141ac0c337ac6d32d31d86ca
Thomas Bye <ThomasBye1@outlook.com>
2019-01-11 19:25:08 -0600

    GUI windows now move properly.

--- MagicalLifeGUIWindows/Input/Specialized Handlers/ContainerDragHandler.cs
+++ MagicalLifeGUIWindows/Input/Specialized Handlers/ContainerDragHandler.cs
@@ -13,12 +13,29 @@ namespace MagicalLifeGUIWindows.Input.Specialized_Handlers
     /// </summary>
     public class ContainerDragHandler
     {
+        /// <summary>
+        /// The last dragged GUI window.
+        /// </summary>
+        private GUIContainer LastDragged;
+
+        /// <summary>
+        /// If true, the last dragged window is still being dragged.
+        /// </summary>
+        private bool StillDraggingLast = false;
+
         public ContainerDragHandler()
         {
+            BoundHandler.MouseListner.MouseDragStart += this.MouseListner_MouseDragStart;
             BoundHandler.MouseListner.MouseDrag += this.MouseListner_MouseDrag;
+            BoundHandler.MouseListner.MouseDragEnd += this.MouseListner_MouseDragEnd;
         }
 
-        private void MouseListner_MouseDrag(object sender, MonoGame.Extended.Input.InputListeners.MouseEventArgs e)
+        private void MouseListner_MouseDragEnd(object sender, MonoGame.Extended.Input.InputListeners.MouseEventArgs e)
+        {
+            this.StillDraggingLast = false;
+        }
+
+        private void MouseListner_MouseDragStart(object sender, MonoGame.Extended.Input.InputListeners.MouseEventArgs e)
         {
             List<GUIContainer> windows = BoundHandler.GUIWindows.FindAll(
                 x => x.DrawingBounds.Contains(e.Position.X, e.Position.Y)
@@ -28,13 +45,28 @@ namespace MagicalLifeGUIWindows.Input.Specialized_Handlers
             if (windows.Count > 0)
             {
                 GUIContainer windowToMove = this.GetHighestPriority(windows);
+                this.LastDragged = windowToMove;
+                this.StillDraggingLast = true;
 
                 Rectangle newPosition = new Rectangle(
-                    new Point((int)(e.Position.X + e.DistanceMoved.X), 
+                    new Point((int)(e.Position.X + e.DistanceMoved.X),
                     (int)(e.Position.Y + e.DistanceMoved.Y)), windowToMove.DrawingBounds.Size);
 
                 windowToMove.DrawingBounds = newPosition;
                 windowToMove.AdjustClickBounds((int)e.DistanceMoved.X, (int)e.DistanceMoved.Y);
+            }
+        }
+
+        private void MouseListner_MouseDrag(object sender, MonoGame.Extended.Input.InputListeners.MouseEventArgs e)
+        {
+            if (StillDraggingLast)
+            {
+                Rectangle newPosition = new Rectangle(
+                    new Point((int)(e.Position.X + e.DistanceMoved.X),
+                    (int)(e.Position.Y + e.DistanceMoved.Y)), this.LastDragged.DrawingBounds.Size);
+
+                this.LastDragged.DrawingBounds = newPosition;
+                this.LastDragged.AdjustClickBounds((int)e.DistanceMoved.X, (int)e.DistanceMoved.Y);
             }
         }
 
