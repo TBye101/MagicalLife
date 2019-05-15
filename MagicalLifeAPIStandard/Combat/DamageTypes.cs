c263397ab79ef8fd4e4a7d5c351f2d3aaf8ddd31
Aaron <a.rcottrill521@gmail.com>
2019-05-15 18:24:15 -0400

    Create the DamageTypes
    Five static readonly types of damage are available now.

---
+++ MagicalLifeAPIStandard/Combat/DamageTypes.cs
@@ -0,0 +1,97 @@
+﻿using MagicalLifeAPI.Properties;
+using MonoGUI.MonoLib.CustomTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicalLifeAPI.Combat
+{
+    public class DamageTypes : Enumeration
+    {
+        public static readonly DamageTypes Poison = new DamageTypes(1, Lang.Poison);
+
+        public static readonly DamageTypes Magic = new DamageTypes(2, Lang.Magic);
+
+        public static readonly DamageTypes Fire = new DamageTypes(3, Lang.Fire);
+
+        public static readonly DamageTypes Crushing = new DamageTypes(4, Lang.Crushing);
+
+        public static readonly DamageTypes Piercing = new DamageTypes(5, Lang.Piercing);
+
+        protected DamageTypes(int id, string name) : base(id, name)
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is DamageTypes))
+            {
+                return false;
+            }
+
+            DamageTypes damageTypes = obj as DamageTypes;
+
+            if (!damageTypes.Name.Equals(this.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            if (damageTypes.Id != Id)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override int GetHashCode()
+        {
+            const int hash = 397;
+            return hash * Id.GetHashCode() * Name.GetHashCode();
+        }
+
+        public static bool operator ==(DamageTypes left, DamageTypes right)
+        {
+            if (left is null)
+            {
+                return right is null;
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(DamageTypes left, DamageTypes right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator <(DamageTypes left, DamageTypes right)
+        {
+            return left is null ? !(right is null) : left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(DamageTypes left, DamageTypes right)
+        {
+            return left is null || left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(DamageTypes left, DamageTypes right)
+        {
+            return !(left is null) && left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(DamageTypes left, DamageTypes right)
+        {
+            return left is null ? right is null : left.CompareTo(right) >= 0;
+        }
+
+
+
+
+    }
+}
---
+++ MonoLib/MonoLib/CustomTypes/Enumeration.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MonoGUI.MonoLib.CustomTypes
+{
+    public abstract class Enumeration : IComparable, IEquatable<Enumeration>
+    {
+        public string Name { get; private set; }
+
+        public int Id { get; private set; }
+
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration
+        {
+            var fields = typeof(T).GetFields(BindingFlags.Public |
+                                             BindingFlags.Static |
+                                             BindingFlags.DeclaredOnly);
+
+            return fields.Select(f => f.GetValue(null)).Cast<T>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Enumeration otherValue))
+                return false;
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public int CompareTo(object obj) => Id.CompareTo(((Enumeration)obj).Id);
+
+        public bool Equals(Enumeration other)
+        {
+            return other != null
+                   && Name == other.Name
+                   && Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1460282102;
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = (hashCode * -1521134295) + Id.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Enumeration left, Enumeration right)
+        {
+            if (left is null)
+            {
+                return right is null;
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Enumeration left, Enumeration right)
+        {
+            return !(left == right);
+        }
+
+        public static bool operator <(Enumeration left, Enumeration right)
+        {
+            return left is null ? !(right is null) : left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(Enumeration left, Enumeration right)
+        {
+            return (left is null) || left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(Enumeration left, Enumeration right)
+        {
+            return !(left is null) && left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(Enumeration left, Enumeration right)
+        {
+            return (left is null) ? (right is null) : left.CompareTo(right) >= 0;
+        }
+
+        // Other utility methods ...
+    }
+}
